664d1e697e91ad3f825f96ea113c16b98a2c2b3a
saksham2105 <solankisaksham1998@gmail.com>
2022-05-24 10:35:50 +0530

    Added unit testcases for suggestion feature

--- src/test/java/com/autosuggestion/engine/engine/EngineApplicationTests.java
+++ src/test/java/com/autosuggestion/engine/engine/EngineApplicationTests.java
@@ -1,13 +1,53 @@
 package com.autosuggestion.engine.engine;
 
+import com.autosuggestion.engine.dao.TrieDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class EngineApplicationTests {
 
+	private String prefix = "Ta";
+
+	private List<String> words;
+
+	@Mock
+	private TrieDAO trieDAO;
+
+	private void initializeWords() {
+		words = new ArrayList<>();
+		words.add("Tank");
+		words.add("Tang");
+		words.add("TanQ");
+		words.add("TanC");
+	}
+
+	@BeforeEach
+	void setup() {
+	  initializeWords();
+	  Mockito.when(trieDAO.suggest(prefix)).thenReturn(words);
+	}
+
 	@Test
-	void contextLoads() {
+	void notNullTest() {
+	  Assertions.assertNotNull(trieDAO);
+	}
+
+	@Test
+	void positiveSuggestionsTest() {
+       Assertions.assertEquals(trieDAO.suggest(prefix).size(),4);
+	}
+
+	@Test
+	void negativeSuggestionsTest() {
+		Assertions.assertFalse(trieDAO.suggest(prefix).size() == 3);
 	}
 
 }
